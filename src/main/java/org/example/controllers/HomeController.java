972fa668bb187cb8945324b9d8ed15d0942e250f
yash-git-max <yashwanthbashipally2005@gmail.com>
2026-04-16 17:37:22 +0530

    HomeController_api_changes

---
+++ src/main/java/org/example/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package org.example.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/api")
+public class HomeController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "App is Running now Thank you use this http//<ip>:8081/swagger-ui/index.html";
+    }
+}
+
